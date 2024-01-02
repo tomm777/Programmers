044402c7179eca7013eb8cf2d5e1212644c01be6
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-02 19:08:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,12 +1,9 @@
 function solution(s) {
     var answer = s.split(' ');
-    // console.log(answer);
     let endAnswer = [];
-    let indexCount = 0;
     let str = " ";
     for(let i = 0; i < answer.length; i++){
         for(let j = 0; j < answer[i].length; j++){
-            
             if(j % 2 === 0){
                 endAnswer.push(answer[i][j].toUpperCase())
             }
@@ -21,10 +18,3 @@ function solution(s) {
     }
     return endAnswer.join('')
 }
-
-// for (let i = 0; i < element.length; i++) {
-//       tmp += i % 2 == 0 ? element[i].toUpperCase() : element[i].toLowerCase();
-//     }
-
-//     answer.push(tmp);
-//     tmp = "";
