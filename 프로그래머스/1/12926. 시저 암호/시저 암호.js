3adf53b48afccd07f28c2a1f8158bb01625aaaa3
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-06 16:55:03 +0900

    [level 1] Title: 시저 암호, Time: 1.73 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,8 @@
+function solution(s, n){
+    return s.split("").map(value => {
+        if (value === " ") return value;
+        return value.toUpperCase().charCodeAt() + n > 90
+        ? String.fromCharCode(value.charCodeAt() + n - 26)
+        : String.fromCharCode(value.charCodeAt() + n)
+    }).join("");
+}
