7a000fda563c0866504954991d6ed4a7502da318
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-10 16:49:06 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,17 +1,8 @@
 function solution(array, commands) {
     var answer = [];
-     for (let i = 0; i < commands.length; i++) {
-        const start = commands[i][0] - 1;
-        const end = commands[i][1];
-        const kth = commands[i][2] - 1;
+    for(let i = 0; i < commands.length; i++){
+        answer.push(array.slice(commands[i][0] - 1, commands[i][1]).sort((a,b) => a - b)[commands[i][2]- 1])
 
-        if (start >= 0 && end <= array.length && kth >= 0) {
-            const slicedArray = array.slice(start, end);
-            const sortedArray = slicedArray.sort((a, b) => a - b);
-            answer.push(sortedArray[kth]);
-        } else {
-            answer.push("Invalid Command");
-        }
     }
     return answer;
 }
