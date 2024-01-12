2f87ca7f90d0703e93f3b3e7547bc9baf064ce79
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-12 16:16:11 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.90 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    let answer = [];
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 0; j < numbers.length; j++){
+            if(numbers[i] === numbers[j] && i === j){
+                continue;
+            }
+            answer.push(numbers[i] + numbers[j])
+            
+        }
+    }
+    const set = new Set(answer.sort());
+    const uniqueArr = [...set];
+    return uniqueArr.sort(function(a,b) {return a - b});
+}
