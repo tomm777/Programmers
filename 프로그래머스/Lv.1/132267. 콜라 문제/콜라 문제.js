4c3e2b35d047923a58d8865b49166cf612660a2d
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-14 20:51:12 +0900

    [level Lv.1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/Lv.1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/Lv.1/132267. 콜라 문제/콜라 문제.js
@@ -1,12 +1,11 @@
 function solution(a, b, n) {
-    let totalCoke = 0;
-    let remainCoke = 0;
+    let answer = 0;
     do{
         // 카운트되는 총 콜라 수
-        totalCoke += Math.floor(n / a) * b;
+        answer += Math.floor(n / a) * b;
         n = Math.floor(n / a) * b + Math.floor(n % a);
     }while(n >= a)
    
     
-    return totalCoke;
+    return answer;
 }
