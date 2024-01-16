ac5bdc4379b34e6c4313d7478b2863ed9a8db879
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-16 20:23:06 +0900

    [level Lv.1] Title: 명예의 전당 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/Lv.1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,22 +1,9 @@
 function solution(k, score) {
-    let answer = [];
-    let arr = [];
-    for(let i = 0; i < score.length; i++){
-        if(arr.length < k){
-            arr.push(score[i]);
-            
-        }
-        else{
-            const minNumber = Math.min(...arr);
-            if(minNumber < score[i]){
-                let indexNum = arr.indexOf(minNumber);
-                arr.splice(indexNum , 1);
-                arr.push(score[i]);
-            }
-        }
-            answer.push(Math.min(...arr))
-        
-        
-    }
-    return answer;
+    var answer = [];
+
+    return score.reduce((acc, cur) => {
+        answer.push(cur);
+        answer = answer.sort((a, b) => b - a).slice(0, k);
+        return [...acc, Math.min(...answer)];
+    }, []);
 }
