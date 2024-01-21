0ab0ff9fb96d42908e72b8e4a03120047a63443b
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-21 16:41:56 +0900

    [level 1] Title: 모의고사, Time: 5.70 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 3.25 ms
+메모리: 36.8 MB, 시간: 5.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 0일 16:33:37
+2024년 1월 0일 16:41:56
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,18 @@
 function solution(answers) {
-  const one = [1, 2, 3, 4, 5];
-  const two = [2, 1, 2, 3, 2, 4, 2, 5];
-  const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  const result = [0, 0, 0];
-  const length = answers.length;
-  for (let i = 0; i < length; i++) {
-    if (one[i % 5] === answers[i]) result[0]++;
-    if (two[i % 8] === answers[i]) result[1]++;
-    if (three[i % 10] === answers[i]) result[2]++;
-  }
-  const answer = [];
-  const maxValue = Math.max(...result);
-  let index = 0;
-  for (let i = 0; i < 3; i++) {
-    if (maxValue === result[i]) {
-      answer[index] = i + 1;
-      index++;
+    let one = [1,2,3,4,5];
+    let two =[2,1,2,3,2,4,2,5];
+    let three = [3,3,1,1,2,2,4,4,5,5]
+    const result = [0, 0, 0];
+    for(let i = 0; i < answers.length; i++){
+        if(one[i % 5] === answers[i]) result[0]++
+         if(two[i % 8] === answers[i] ) result[1]++
+         if(three[i % 10] ===  answers[i]) result[2]++
     }
-  }
-  return answer;
+    const answer = [];
+    const maxCount = Math.max(...result);
+    for(let i = 0; i < 3; i++){
+        console.log(result[i])
+        if(result[i] === maxCount) answer.push(i + 1)
+    }
+    return answer
 }
