8a47ef46a0da3eab856cd92e6157109538c288c8
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-23 21:17:23 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.94 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,12 +1,10 @@
 function solution(number, limit, power) {
     var answer = 0;
-    for (let i = 1; i <= number; i++)
-    {
+    for (let i = 1; i <= number; i++){
         let count = 0;
-        for (let j = 1; j * j <= i; j++)
-        {
-            if (j * j == i) count++;
-            else if (i % j == 0) count += 2;
+        for (let j = 1; j * j <= i; j++){
+            if (j * j === i) count++;
+            else if (i % j === 0) count += 2;
         }
         if (count > limit) count = power;
         answer += count;
