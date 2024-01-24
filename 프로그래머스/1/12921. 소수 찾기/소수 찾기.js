753472811c8b25b3590f2ab69cc73d53b5794056
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-01-24 20:14:08 +0900

    [level 1] Title: 소수 찾기, Time: 30.12 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,18 +1,16 @@
-function countPrimes(n) {
+function solution(n) {
   const isPrime = Array(n + 1).fill(true);
   isPrime[0] = isPrime[1] = false;
+    let arr = [];
 
   for (let i = 2; i * i <= n; i++) {
     if (isPrime[i]) {
       for (let j = i * i; j <= n; j += i) {
+          
         isPrime[j] = false;
       }
     }
   }
-
+    console.log(isPrime)
   return isPrime.filter((value) => value).length;
-}
-
-function solution(n) {
-  return countPrimes(n);
 }
