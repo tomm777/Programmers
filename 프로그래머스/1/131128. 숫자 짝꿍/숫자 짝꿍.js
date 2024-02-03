d8de8142a48734e858aca239d2d47c7a75b283c0
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-03 16:11:59 +0900

    [level 1] Title: 숫자 짝꿍, Time: 2.96 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -5,6 +5,7 @@ function solution(X, Y) {
     
     for(let i = 0 ; i < 10 ; i ++) {
         const curX = X.filter(a => Number(a) === i).length
+        console.log(curX)
         const curY = Y.filter(a => Number(a) === i).length
         answer+=String(i).repeat(Math.min(curX, curY))
     }
