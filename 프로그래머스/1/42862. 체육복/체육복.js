6b561324642f3a8cc9f9612460d498740a901e2d
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-06 19:11:36 +0900

    [level 1] Title: 체육복, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -2,20 +2,18 @@ function solution(n, lost, reserve) {
     
     let answer = 0;
     
-    // 잃어버리지 않은 사람 번호
+    // 잃어버린 사람 번호
     const noReserveLost = lost.sort((a,b) => a-b).filter((lost) => !reserve.includes(lost));
     
+    // 여벌 체육복이 있는 사람 번호
     let hasReserve = reserve.sort((a,b) => a-b).filter((reverse) => !lost.includes(reverse));
-    // console.log(noReserveLost,hasReserve)
-    
     const finalLost = noReserveLost.filter((lost) => {
-        
-        const lend = hasReserve.find((reserve) => Math.abs(reserve - lost) == 1);
-        console.log(lend)
-        // console.log(Math.abs(reserve - lost))
+        // 여벌 체육복이 있는 학생중 빌려줄 학생들 하나씩 제외
+        const lend = hasReserve.find((reserve) => {
+            return Math.abs(reserve - lost) === 1
+        });
         
         if(!lend) return lost;
-        
         hasReserve = hasReserve.filter((reverse) => reverse !== lend);
     })
     answer = n - finalLost.length;
