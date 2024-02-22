221887d36e88760a3cbee52d3619878fe3a469a2
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-22 19:24:55 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.80 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,40 +1,58 @@
 function solution(numbers, hand) {
-    const keypad = {
-        1: [0, 0], 2: [0, 1], 3: [0, 2],
-        4: [1, 0], 5: [1, 1], 6: [1, 2],
-        7: [2, 0], 8: [2, 1], 9: [2, 2],
-        '*': [3, 0], 0: [3, 1], '#': [3, 2]
-    };
-
-    let leftHand = '*';
-    let rightHand = '#';
-    let answer = '';
-
-    const getDistance = (handPos, targetPos) => {
-        return Math.abs(handPos[0] - targetPos[0]) + Math.abs(handPos[1] - targetPos[1]);
-    };
-
-    for (let num of numbers) {
-        if (num === 1 || num === 4 || num === 7) {
-            leftHand = num;
-            answer += 'L';
-        } else if (num === 3 || num === 6 || num === 9) {
-            rightHand = num;
-            answer += 'R';
-        } else {
-            const targetPos = keypad[num];
-            const leftDistance = getDistance(keypad[leftHand], targetPos);
-            const rightDistance = getDistance(keypad[rightHand], targetPos);
-
-            if (leftDistance < rightDistance || (leftDistance === rightDistance && hand === 'left')) {
-                leftHand = num;
-                answer += 'L';
-            } else if (leftDistance > rightDistance || (leftDistance === rightDistance && hand === 'right')) {
-                rightHand = num;
-                answer += 'R';
+    let answer = [];
+    // numbers를 인덱싱하기위한 배열
+    const arr = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        ['*',0,'#']];
+    // *과 # 의 인덱스 부여
+    let rightHand = '32';
+    let leftHand = '30';
+    let targetHand = '';
+    // numbers의 length만큼 순회
+    for(let i = 0; i < numbers.length; i++){
+        // [1,2,3] [4,5,6] 배열을 순회하면서 
+        for(let j = 0; j < arr.length; j++){
+            if(arr[j].indexOf(numbers[i]) === 0){
+                leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                answer.push('L');
+            }
+            else if(arr[j].indexOf(numbers[i]) === 2){
+                rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                answer.push('R');
+            }
+            else {
+                if(arr[j].indexOf(numbers[i]) > 0){
+                    targetHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                const leftHandDif = Math.abs(Number(targetHand[0])-Number(leftHand[0])) + Math.abs(Number(targetHand[1])-Number(leftHand[1]));
+                const rightHandDif = Math.abs(Number(targetHand[0])-Number(rightHand[0])) + Math.abs(Number(targetHand[1])-Number(rightHand[1]));
+                    if(leftHandDif < rightHandDif){
+                        leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                        answer.push('L');
+                    }
+                    else if(leftHandDif > rightHandDif){
+                        rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                        answer.push('R');
+                        break;
+                    }
+                    else {
+                        if(hand === 'right'){
+                            rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                            answer.push('R');
+                            break
+                        }
+                        else {
+                            leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
+                            answer.push('L');
+                            break;
+                        }
+                        
+                    }
+                }
             }
         }
     }
 
-    return answer;
+    return answer.join('');
 }
