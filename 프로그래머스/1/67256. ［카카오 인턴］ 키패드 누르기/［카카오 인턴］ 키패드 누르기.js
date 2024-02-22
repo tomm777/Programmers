e16f5805060becfb49aa175a8ef46db2818abc8a
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-22 20:16:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,5 +1,5 @@
 function solution(numbers, hand) {
-    let answer = [];
+    let answer = '';
     // numbers를 인덱싱하기위한 배열
     const arr = [
         [1,2,3],
@@ -7,55 +7,51 @@ function solution(numbers, hand) {
         [7,8,9],
         ['*',0,'#']];
     // *과 # 의 인덱스 부여
-    let rightHand = '32';
-    let leftHand = '30';
+    let rightHandIndex = '32';
+    let leftHandIndex = '30';
     let targetHand = '';
+    // 거리를 계산해주는 함수
+    const getDistance = (handIdx, targetIdx) => {
+        return Math.abs(handIdx[0] - targetIdx[0]) + Math.abs(handIdx[1] - targetIdx[1]);
+    };
     // numbers의 length만큼 순회
+    
     for(let i = 0; i < numbers.length; i++){
-        // [1,2,3] [4,5,6] 배열을 순회하면서 
+            let targetIndex; 
+        // [1,2,3] [4,5,6] 배열을 순회하면서 인덱싱
         for(let j = 0; j < arr.length; j++){
-            if(arr[j].indexOf(numbers[i]) === 0){
-                leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                answer.push('L');
+            let index = arr[j].indexOf(numbers[i]);
+            // 해당 인덱스를 찾았을 때 해당 위치를 할당 후 탈출 
+            if(index !== -1){
+                targetIndex = [j, index];
                 break;
-            }
-            else if(arr[j].indexOf(numbers[i]) === 2){
-                rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                answer.push('R');
-                break;
-            }
-            else {
-                console.log(arr[j].indexOf(numbers[i]))
-                if(arr[j].indexOf(numbers[i]) > 0){
-                    targetHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                const leftHandDif = Math.abs(Number(targetHand[0])-Number(leftHand[0])) + Math.abs(Number(targetHand[1])-Number(leftHand[1]));
-                const rightHandDif = Math.abs(Number(targetHand[0])-Number(rightHand[0])) + Math.abs(Number(targetHand[1])-Number(rightHand[1]));
-                    if(leftHandDif < rightHandDif){
-                        leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                        answer.push('L');
-                    }
-                    else if(leftHandDif > rightHandDif){
-                        rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                        answer.push('R');
-                        break;
-                    }
-                    else {
-                        if(hand === 'right'){
-                            rightHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                            answer.push('R');
-                            break
-                        }
-                        else {
-                            leftHand = [j] +""+ arr[j].indexOf(numbers[i]);
-                            answer.push('L');
-                            break;
-                        }
-                        
-                    }
-                }
             }
         }
-    }
+        // 1, 4, 7 일 때 L
+        if(numbers[i] === 1 || numbers[i] === 4 || numbers[i] === 7){
+            leftHandIndex = targetIndex;
+            answer += 'L';
+        }
+        // 3, 6, 9 일 때 R
+        else if(numbers[i] === 3  || numbers[i] === 6 || numbers[i] === 9){
+            rightHandIndex = targetIndex;
+            answer += 'R';
+        }
+        // 중간 숫자일 경우
+        else {
+             const leftDistance = getDistance(leftHandIndex, targetIndex);
+             const rightDistance = getDistance(rightHandIndex, targetIndex);
+            if(leftDistance < rightDistance || leftDistance === rightDistance && hand === 'left'){
+                leftHandIndex = targetIndex;
+                answer += 'L'
+            }
+            else if(leftDistance > rightDistance || leftDistance === rightDistance && hand === 'right'){
+                rightHandIndex = targetIndex;
+                answer += 'R'
+            }
 
-    return answer.join('');
+    }
+}
+
+    return answer;
 }
