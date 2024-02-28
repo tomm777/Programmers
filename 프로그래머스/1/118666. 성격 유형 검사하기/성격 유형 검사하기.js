dc32205f2c6b11a56c1f8dcf89fcf99458ac1925
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-28 20:39:22 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 5.73 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,36 +1,15 @@
 function solution(survey, choices) {
-    var answer = '';
-    const checkScore = {
-        1 : 3,
-        2 : 2,
-        3 : 1,
-        4 : 0,
-        5 : -1,
-        6 : -2,
-        7 : -3
-    }
-    const types = {
-        R : 0,
-        T : 0,
-        C : 0,
-        F : 0,
-        J : 0,
-        M : 0,
-        A : 0,
-        N : 0
-    }
-    // 3 3 -1
+    const types = {R : 0, T : 0, C : 0, F : 0, J : 0, M : 0, A : 0, N : 0}
     for(let i = 0; i < survey.length; i++){
-        // console.log(checkScore[choices[i]]);
-        // console.log(survey[i])
-        let score = checkScore[choices[i]];
-        if(score > 0){
-            types[survey[i][0]] += score;
+        let score2 = choices[i] - 4;
+        if(score2 < 0){
+            types[survey[i][0]] += Math.abs(score2);
         }
-        else if(score < 0){
-            types[survey[i][1]] += Math.abs(score);
+        else if(score2 > 0){
+            types[survey[i][1]] += Math.abs(score2);
         }
     }
+    console.log(types)
     return (types['R'] < types['T'] ? 'T' : 'R') +
         (types['C'] < types['F'] ? 'F' : 'C') +
     (types['J'] < types['M'] ? 'M' : 'J') +
