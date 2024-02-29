e183cc1df244ebdaa5a489fecec324af5e67e63b
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-02-29 21:32:13 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990#) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 1.92 ms
+메모리: 33.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 29일 21:17:43
+2024년 02월 29일 21:32:10
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,23 +1,17 @@
 function solution(wallpaper) {
     let S = [];
     let E = [];
-    let test = [];
-    // ------------------------------------------
-    // let left = [];
-
+    let fileIndex = [];
     for(let i = 0; i < wallpaper.length; i++){
         if(wallpaper[i].includes('#')){
             S.push(wallpaper[i].indexOf('#').toString())
             E.push(wallpaper[i].lastIndexOf('#').toString())
-            test.push(i)
+            fileIndex.push(i)
         }
     }
-    let answer = (Math.min(...S).toString() + (Math.max(...test) + 1) + (Math.max(...E) + 1)).split('')
-    let left = Math.min(...test);
+    let left = Math.min(...fileIndex);
     let top = Math.min(...S)
-    let right = Math.max(...test) + 1;
+    let right = Math.max(...fileIndex) + 1;
     let bottom = Math.max(...E) + 1;
-    // answer.map(item => Number(item))
-    console.log()
     return [left,top,right,bottom]
 }
