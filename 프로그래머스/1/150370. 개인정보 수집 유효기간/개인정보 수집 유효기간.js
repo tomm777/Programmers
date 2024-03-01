2eb6588d7314033f2ade15bf79371c16c82cedb3
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-01 15:42:44 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.64 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,7 +1,7 @@
 function solution(today, terms, privacies) {
     const term = {}
+    const answer = [];    
     terms.forEach(item => term[item.split(' ')[0]] = item.split(' ')[1])
-    const answer = [];
     for(let i = 0; i < privacies.length; i++){
         let date = privacies[i].split('.')
         let targetDays = term[date[2][3]] * 28
@@ -12,6 +12,7 @@ function solution(today, terms, privacies) {
         if (day > 28) {
             month += Math.floor(day / 28);
             day = day % 28;
+        }
         if (day === 0) {
             day = 28;
             month -= 1;
@@ -27,9 +28,6 @@ function solution(today, terms, privacies) {
             if(new Date(`${year}.${month}.${day}`) < new Date(today)){
                 answer.push(i + 1)
             }
-        
-    }
-       
     }
 
     return answer;
