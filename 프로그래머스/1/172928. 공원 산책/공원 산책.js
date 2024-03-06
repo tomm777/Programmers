b57789cdc3032089bc971a0f5ad3059527c36b0e
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-06 22:22:25 +0900

    [level 1] Title: 공원 산책, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,12 +1,13 @@
 function solution(park, routes) {
     let startLocation = [];
+    // S의 위치를 찾아서 대입
     for(let i = 0; i < park.length; i++){
         if(park[i].indexOf('S') !== -1){
             startLocation = [i, park[i].indexOf('S')];
             break;
         }
     }
-    // 성공인지 실패인지 알려주는 함수
+    // 성공일때 그에 맞는 위치, 실패일 때 원래의 위치를 반환하는 함수
     const moveLocation = (currentPoint, direction, num) => {
     const [x, y] = currentPoint;
     let newX = x, newY = y;
@@ -16,27 +17,16 @@ function solution(park, routes) {
         else if (direction === 'W') newY -= 1;
         else if (direction === 'S') newX += 1;
         else if (direction === 'N') newX -= 1;
-
         if (!park[newX] || !park[newX][newY] || park[newX][newY] === 'X') {
             return currentPoint;
         }
     }
-
     return [newX, newY];
 };
-
     for(let i = 0; i < routes.length; i++){
-        let direction = routes[i].split(' ')[0];
-        let number = routes[i].split(' ')[1];
-        let result = moveLocation([startLocation[0],startLocation[1]],direction,Number(number));
-        if(result !== undefined){
-            startLocation[0] = result[0];
-            startLocation[1] = result[1]
-        }
-        else {
-            continue;
-        }
+        let [direction, number] = routes[i].split(' ');
+        startLocation = moveLocation([startLocation[0],startLocation[1]],direction,Number(number));
         
     }
-    return [startLocation[0], startLocation[1]];
+    return startLocation
 }
