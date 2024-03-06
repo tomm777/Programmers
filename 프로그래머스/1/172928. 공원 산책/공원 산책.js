7a28656af8d62afb8295c55c4338f2a889742b00
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-06 22:19:50 +0900

    [level 1] Title: 공원 산책, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,52 +1,35 @@
 function solution(park, routes) {
-    var answer = [];
     let startLocation = [];
-    let Obstacle = [];
-    let field = [];
     for(let i = 0; i < park.length; i++){
         if(park[i].indexOf('S') !== -1){
-            startLocation.push((i))
-            startLocation.push(park[i].indexOf('S'));
+            startLocation = [i, park[i].indexOf('S')];
             break;
         }
     }
     // 성공인지 실패인지 알려주는 함수
-    const moveLocation = (currentPoint,direction,num) => {
-        // console.log(currentPoint,direction,num)
-        // console.log("OPERATION")
-        num = Number(num);
-        for(let i = 1; i <= num; i++){
-            let point = '';
-            if(direction === 'E'){
-                point = park[currentPoint[0]]?.[currentPoint[1] + i]
-            }
-            else if(direction === 'N'){
-                point = park[currentPoint[0] - i]?.[currentPoint[1]]
-            }
-            else if(direction === 'W'){
-                point = park[currentPoint[0]]?.[currentPoint[1]  - i]
-            }
-            else if(direction === 'S'){
-                point = park[currentPoint[0] + i]?.[currentPoint[1]]
-            }
-            if(point === 'X' || point === undefined){
-                return;
-            }
-            
-        }
-        switch(direction){
-            case 'E' : return [currentPoint[0],currentPoint[1] + num]
-            case 'W' : return [currentPoint[0],currentPoint[1]  - num]
-            case 'S' : return [currentPoint[0] + num, currentPoint[1]]
-            case 'N' : return [currentPoint[0] - num, currentPoint[1]]
+    const moveLocation = (currentPoint, direction, num) => {
+    const [x, y] = currentPoint;
+    let newX = x, newY = y;
+
+    for (let i = 1; i <= num; i++) {
+        if (direction === 'E') newY += 1;
+        else if (direction === 'W') newY -= 1;
+        else if (direction === 'S') newX += 1;
+        else if (direction === 'N') newX -= 1;
+
+        if (!park[newX] || !park[newX][newY] || park[newX][newY] === 'X') {
+            return currentPoint;
         }
     }
+
+    return [newX, newY];
+};
+
     for(let i = 0; i < routes.length; i++){
         let direction = routes[i].split(' ')[0];
         let number = routes[i].split(' ')[1];
-        let result = moveLocation([startLocation[0],startLocation[1]],direction,number);
+        let result = moveLocation([startLocation[0],startLocation[1]],direction,Number(number));
         if(result !== undefined){
-            console.log(result)
             startLocation[0] = result[0];
             startLocation[1] = result[1]
         }
