d4b2a90827f619122dbcd0a5e1eaf6ebc871a712
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-13 17:45:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -4,7 +4,8 @@ function solution(id_list, report, k) {
     const report_list = {}
     
     id_list.map((user)=>{
-        report_list[user] = [] //key로 userid를 value로 빈 배열을 가지는 객체
+        //key로 userid를 value로 빈 배열을 가지는 객체
+        report_list[user] = [] 
     })
      report.map((user)=>{
         const [user_id, report_id] = user.split(' ')
@@ -13,7 +14,8 @@ function solution(id_list, report, k) {
         }        
     })
      for(const key in report_list){
-        if(report_list[key].length >= k){ //이용정지 유저
+        //이용정지 유저
+        if(report_list[key].length >= k){ 
             report_list[key].map((user)=>{
                 answer[id_list.indexOf(user)] += 1
             })
