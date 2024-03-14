45a22d09f24e93c22147cc77c09b4d21f7d1f47b
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-14 19:04:38 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 6.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,36 @@
+function solution(bandage, health, attacks) {
+    // 마지막 공격
+    const lastAttack = attacks[attacks.length - 1][0];
+    // 현재 체력
+    let currentHealth = health;
+    //  시전시간   초당 회복량  추가회복량
+    let [workTime,recoveryPS,addHealth] = bandage;
+    // 연속 공격 카운팅 변수
+    let count = 0;
+    // 공격 시간 인덱스
+    let index = 0;
+    for(let i = 0; i <= lastAttack; i++){
+        if(attacks[0][0] === i){
+            currentHealth -= attacks[0][1]
+            attacks.splice(0,1);
+            console.log(attacks)
+            count = 0;
+        }
+        else {
+            count++
+            if(count >= 1){
+                currentHealth += recoveryPS;
+                if(count === workTime) {
+                    currentHealth += addHealth;
+                    count = 0;
+                }
+                if(currentHealth > health) currentHealth = health;
+            }
+        }
+         if (currentHealth <= 0) {
+            return -1; // 체력이 0 이하면 즉시 종료
+        }
+    }
+    
+    return currentHealth
+}
