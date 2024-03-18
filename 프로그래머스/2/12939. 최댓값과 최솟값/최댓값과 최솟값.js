e49cb83b3577172f884e9cac6dd04bc1df25da46
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-18 18:36:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,3 @@
 function solution(s) {
-    var answer = '';
-    const max = Math.max(...s.split(' '));
-    const min = Math.min(...s.split(' '))
-    
     return  Math.min(...s.split(' ')) +' '+ Math.max(...s.split(' '))
 }
