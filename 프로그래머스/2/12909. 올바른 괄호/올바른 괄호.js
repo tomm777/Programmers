4c8da3cd7795c9fbb23052a6b008d1f024c96467
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-20 21:21:11 +0900

    [level 2] Title: 올바른 괄호, Time: 22.40 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,12 @@
 function solution(s){
     let count = 0;
-    // if(s.length % 2 !== 0){
-    //     return false
-    // }
     for(let i = 0; i < s.length; i++){
         // 첫번째에 (가 있을때 바로 false 처리
         if(s[0] === ')'){
            return false
         }
-        if(count < 0 ){
+        // (보다 )이 더 많은 시점에 false 처리
+        if(count < 0){
             return false
         }
         count += s[i] === '(' ? 1 : -1
