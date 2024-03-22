c1b8a7f5adc68feecdd6586c1977c59753bda6c1
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-22 16:30:49 +0900

    [level 2] Title: 숫자의 표현, Time: 0.28 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,11 +1,11 @@
 function solution(n) {
     var answer = 0;
     let count = 1;
-    let test = [];
+    let arr = [];
     
     for(let i = 1; i <= n; i++){
-        test.push(i)
-        const sum = test.reduce((acc, cur) => acc + cur, 0);
+        arr.push(i)
+        const sum = arr.reduce((acc, cur) => acc + cur, 0);
         if(i > 1){
             if((n - sum) % i === 0 && sum <= n){
                 count++;
