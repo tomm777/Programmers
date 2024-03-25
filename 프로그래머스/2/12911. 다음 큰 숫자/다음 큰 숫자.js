1e8c8e4a912ebd878fedaa1109694e355ae90279
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-25 18:16:05 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,7 +1,6 @@
 function solution(n) {
-    // var answer = 0;
     let length = n.toString(2).split('').filter(item => item === '1').length;
-    let answer = n + 1
+    let answer = n + 1;
     while(answer.toString(2).split('').filter(item => item === '1').length !== length){
         answer += 1
     }
