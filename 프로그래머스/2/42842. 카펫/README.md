cf4e6eeffdb08e686b45802582a786387e97a3f8
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-03-28 20:05:59 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 28일 19:58:49
+2024년 03월 28일 20:05:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,24 +1,11 @@
 function solution(brown, yellow) {
-    var answer = [];
-    let arr = [];
-    // (brown + yellow) / x = y
-    // (12) / 2,4,6,12 = y (6,3,2,1);
-    
-    
-    // (x-2) * (y-2) === yellow
-    
-    
     let sum = brown + yellow
-    for(let i = 2; i <= sum; i++){
-        if(i === sum) break;
-        if(sum % i === 0){
-            arr.push(i)
-        }
-    for(let i = 0; i < arr.length; i++){
-        let x = sum / arr[i];
-        if((x -2) * (arr[i] - 2) === yellow){
-            return [x,arr[i]]
+    for(let height = 3; height <= brown; height++){
+        if(sum % height === 0){
+            let width = sum / height;
+            if((width - 2) * (height - 2) === yellow){
+                return [width,height]
+            }
         }
     }
-}
 }
