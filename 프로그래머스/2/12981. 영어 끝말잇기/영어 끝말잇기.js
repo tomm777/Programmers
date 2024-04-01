3afd10b63eee741e6849cc9cdff0cb8598c32bb7
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-04-01 18:57:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,8 +1,7 @@
 function solution(n, words) {
-    var answer = [];
+    let arr = [];
     let count = 0;
     let sequence = 0;
-    let checkNum = 0;
     for(let i = 0; i < words.length; i++){
         let length = words[i].length;
         // 사람의 차례
@@ -15,23 +14,18 @@ function solution(n, words) {
             break;
         }
         // 중복인 단어를 말할 때
-        if(answer.includes(words[i])){
+        if(arr.includes(words[i])){
             break;
         }
-        // if(answer[answer.length - 1][])
         // 저장된 배열이 1개 이상일 때
-        if(answer.length > 0){
-            if(answer[answer.length - 1][answer[answer.length - 1].length - 1] !== words[i][0]){
+        if(arr.length > 0){
+            if(arr[arr.length - 1][arr[arr.length - 1].length - 1] !== words[i][0]){
                 break;
 }
              
         }
-        answer.push(words[i]);
-        
-        // 돌아가는 차례 순회 카운팅
-        // 중복단어가 있을 때
-        // 비교할 다음 문자가 있을 때
+        arr.push(words[i]);
         
     }
-    return answer.length === words.length ? [0,0] : [sequence,count]
+    return arr.length === words.length ? [0,0] : [sequence,count]
 }
