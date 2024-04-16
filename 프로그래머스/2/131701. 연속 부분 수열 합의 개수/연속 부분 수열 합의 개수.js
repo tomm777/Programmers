8f4e64ca37e2df9381a7051c16b06c28fda935a6
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-04-16 21:39:23 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 94.70 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,19 +1,15 @@
 function solution(elements) {
   const set = new Set(elements); 
-  const n = elements.length; 
+  const length = elements.length; 
   
   // elements 배열에 시작위치를 알려주는 for문
-  for(let i=0; i<n; i++){
-    // 합계 저장
-    let sum = 0;
-    // 연속 부분 수열 시작위치부터 i+n까지 순회
-    // ex) 0에서 시작하면 4까지, 4에서 시작하면 8까지
-    for(let j=i; j<i+n; j++){
-      // % 인덱스를 순환하면서 sum에 더함
-      sum += elements[j%n];
-      set.add(sum)
+    for(let i = 0; i < length; i++){
+        let sum = 0;
+        for(let j = i; j < length+i; j++){
+            // set객체는 SameValueZero 알고리즘을 사용하여 중복된 값이 할당되지 않게함
+            sum += elements[j % length]
+            set.add(sum)
+        }
     }
-
-  }
   return set.size
 }
