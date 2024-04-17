360fd7b2cb347be9320222e4872a84e69fb61640
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-04-17 22:54:18 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,23 +1,15 @@
 function solution(s) {
-    let count = 0;
     if(s.length % 2 !== 0) return 0
-    const obj = {
-        '[' : ']',
-        '{' : '}',
-        '(' : ')'
-    }
+    let count = 0;
+    const obj = {'[' : ']','{' : '}','(' : ')'}
     for(let i = 0; i < s.length; i++){
         let str = s.slice(i) + s.slice(0, i);
         const arr = [];
         let flag = 1;
         for(let j = 0; j < str.length; j++){
-            // if (str[j] === "(" || str[j] === "{" || str[j] === "[") arr.push(str[j]);
-            if(str[j] === "(" || str[j] === "{" || str[j] === "[") arr.push(str[j]);
+            if(obj[str[j]]) arr.push(str[j])
             else {
-                const temp = arr.pop();
-                if(str[j] === ')'&& temp === '(') continue;
-                else if(str[j] === '}'&& temp === '{') continue;
-                else if(str[j] === ']'&& temp === '[') continue;
+                if(str[j] === obj[arr.pop()]) continue;
                 flag = 0;
                 break;
             }
