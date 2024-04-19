6713cc8dfa54cd4590cc9d776d3eeb20ef678fd3
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-04-19 17:20:15 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,8 +1,8 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a); // 내림차순 정렬
     let hIndex = 0;
-    while (hIndex < citations.length && citations[hIndex] > hIndex) {
-        hIndex++;
+    citations.sort((a, b) => b - a); // 내림차순 정렬    
+    while(hIndex < citations.length && citations[hIndex] > hIndex){
+        hIndex++
     }
-    return hIndex; // H-Index 반환
+    return hIndex
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 19일 17:18:06
+2024년 04월 19일 17:20:14
 
 ### 문제 설명
 
