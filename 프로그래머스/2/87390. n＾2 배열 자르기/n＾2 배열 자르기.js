5bbb9123fc84a6d99371e3a93b8045a03616e48d
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-04-22 20:49:17 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.86 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,14 +1,11 @@
 function solution(n, left, right) {
     let answer = [];
-
     for (let index = left; index <= right; index++) {
         // 2차원 배열의 행과 열의 인덱스로 변환
         let i = Math.floor(index / n);
         let j = index % n;
-
         // 직접 해당 위치의 값을 계산하여 추가
         answer.push(Math.max(i, j) + 1);
     }
-
     return answer;
 }
