ab3a639ba85b48626c85d31ec1438e739e014f1b
Gibeom Kim <95726595+tomm777@users.noreply.github.com>
2024-05-02 19:28:45 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,15 @@
 function solution(progresses, speeds) {
     const answer = [];
     let count = 1;
-    // 5 10 1 1 20 1
-    // 5 : 1
-    // 10 : 3
-    // 20 : 2
-    let test = Math.ceil((100 - progresses[0]) / speeds[0])
-    // answer.push(test)
+    let firstValue = Math.ceil((100 - progresses[0]) / speeds[0])
     for(let i = 1; i <= progresses.length; i++){
-        
-        if(test >= Math.ceil((100 - progresses[i]) / speeds[i])){
+        if(firstValue >= Math.ceil((100 - progresses[i]) / speeds[i])){
             count++
         }else {
             answer.push(count);
             count = 1;
-            test = Math.ceil((100 - progresses[i]) / speeds[i]);
+            firstValue = Math.ceil((100 - progresses[i]) / speeds[i]);
         }
-        
     }
     return answer;
 }
